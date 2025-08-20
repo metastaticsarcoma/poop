aef026e36993f367f0858043076f67515128c2a7
(renal) sarcoma <acts0fm1s3ry@gmail.com>
2025-08-19 18:52:52 -0600

    Update bot.py
    fixed and summarized code.

--- bot.py
+++ bot.py
@@ -14,29 +14,22 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
+
+    joke_dict = {
+        "joke1": "My grandpa always said when one door closes, another one opens. Smart man but a horrible cabinet maker.",
+        "joke2": "People are usually shocked when they find out I'm not a very good electrician.",
+        "joke3": "I once got stuck in an elevator. Now I take steps to avoid them!",
+        "joke4": "My doctor recommended I buy orthopedic insoles. I didn't think they would work, but now I stand corrected",
+    }
+
     if message.content.startswith('$hello'):
         await message.channel.send("Hi!")
     elif message.content.startswith('$bye'):
         await message.channel.send("\U0001f642")
-    else:
-        await message.channel.send(message.content)
-
-@client.event
-async def on_message(message):
-    joke_dict = {
-            "joke1": "My grandpa always said when one door closes, another one opens. Smart man but a horrible cabinet maker.",
-            "joke2": "People are usually shocked when they find out I'm not a very good electrician.",
-            "joke3": "I once got stuck in an elevator. Now I take steps to avoid them!",
-            "joke4": "My doctor recommended I buy orthopedic insoles. I didn't think they would work, but now I stand corrected",
-            }
-    random_value = random.choice(list(joke_dict.values()))
-    if message.author == client.user:
-        return
-    if message.content.startswith('$tellajoke'):
+    elif message.content.startswith('$tellajoke'):
+        random_value = random.choice(list(joke_dict.values()))
         await message.channel.send(random_value)
-    elif message.content.startswith('$bye'):
-        await message.channel.send("\U0001f642")
     else:
-        await message.channel.send(message.content)    
+        await message.channel.send(message.content) 
 
 client.run("")
